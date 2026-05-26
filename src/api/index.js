f5e6853dbecc6003a5e687af513783a034f07a0a
Ehorga <yehornahiba1207@gmail.com>
2026-05-26 15:36:31 +0300

    Blog1

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const httpClient = axios.create({
+    baseURL: "https://dummyjson.com",});
+    export const loginUser = (dataUser) => httpClient.post("/auth/login", dataUser);
---
+++ src/store/userSlice.js
@@ -0,0 +1,14 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name : "user",
+    initialState : {
+        user : null,
+        error : null,
+        isPending : false,
+    },
+    reducers : { },
+    extraReducers : () => {},
+});
+
+export default userSlice.reducer;
